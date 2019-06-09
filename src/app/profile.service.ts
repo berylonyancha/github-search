0110aa04038fd85d3f5b62dba83da731c425c436
Beryl Onyancha <berylonyancha@gmail>
2019-06-10 00:48:07 +0300

    Creat functions grtProfileDesc,getProfileRepos,updateProfile.

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient} from '@angular/common/http';
+
 
 @Injectable()
 export class ProfileService {
+  private username:string;
+  private clientid = '75fbc3436ae7b2cacc1c';
+  private clientsecret = '3865052f662ff45e88db544725eedad0c1424758';
 
-  private username: string;
-  constructor(private http:Http) { }
- 
+  constructor(private http:HttpClient) { 
+    this.username = 'berylonyancha'; 
+
+  }
+  getProfileDesc(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  
+  }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  
+  }
+  updateProfile(username:string){
+    this.username = username;
+  }
+
 }
