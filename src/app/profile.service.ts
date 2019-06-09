1501160d869d2bb916515ffa8cdde54324fd9e07
Beryl Onyancha <berylonyancha@gmail>
2019-06-09 16:37:54 +0300

    Import http, create a constructor

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  constructor(private http:Http) { }
+ 
 }
